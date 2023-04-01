6aa79de3f2771e639a0bc680dd43f2642ffe3ab2
azhargithub <azharthread@gmail.com>
2023-04-01 16:51:58 +0530

    new project for order

---
+++ src/main/java/com/azhar/TestApiGitApplication.java
@@ -0,0 +1,13 @@
+package com.azhar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApiGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azhar/TestApiGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azhar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
